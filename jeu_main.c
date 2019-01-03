cb9338013b9d6c9de1e1ff27d180e95023768a74
SharkEzz <icraft640@gmail.com>
2019-01-03 21:18:30 +0100

    Little fix and introducing the game guide

--- jeu_main.c
+++ jeu_main.c
@@ -3,7 +3,15 @@
 #include "jeu_main.h"
 
 int main(int argc, char *argv[])
-{;
+{
+
+    if(argv[1] == "-h")
+        help();
 
     return 0;
+}
+
+void help()
+{
+    printf("Ceci est l'aide");
 }
--- jeu_main.h
+++ jeu_main.h
@@ -1,6 +1,7 @@
 #ifndef JEU_MAIN_H
 #define JEU_MAIN_H
 
+void help();
 
 
 #endif
